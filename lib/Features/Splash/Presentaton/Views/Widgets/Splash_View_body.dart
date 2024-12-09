0bcd757c4974cd49de0c598fd2c4277954f182a3
reem <reemmohmedmohmed@gmail.com>
2024-12-09 10:06:37 +0200

    complite screen splash and adding animation for it Splash is done!

--- lib/Features/Splash/Presentaton/Views/Widgets/Splash_View_body.dart
+++ lib/Features/Splash/Presentaton/Views/Widgets/Splash_View_body.dart
@@ -1,12 +1,48 @@
+import 'package:bookly/Features/Splash/Presentaton/Views/Widgets/sliding_animation.dart';
+import 'package:bookly/core/Utils/assets_data.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    slidingAnimation = Tween(begin: Offset(0, 2), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 5,
+        ),
+        SlidingAnimation(slidingAnimation: slidingAnimation),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/Features/Splash/Presentaton/Views/Widgets/constant.dart';
 import 'package:bookly/Features/Splash/Presentaton/Views/splash_view.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -11,7 +13,9 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
       home: SplashView(),
     );
   }
