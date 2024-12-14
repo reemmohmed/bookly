4bf6c70ba2cceb05e321649de08f8d1b1c4fcfef
reem <reemmohmedmohmed@gmail.com>
2024-12-14 11:18:06 +0200

    add titel and rating to delatsview

--- lib/Features/Home/presentaion/Views/widgets/book_rating.dart
+++ lib/Features/Home/presentaion/Views/widgets/book_rating.dart
@@ -4,14 +4,17 @@ import 'package:font_awesome_icon_class/font_awesome_icon_class.dart';
 import '../../../../../core/Utils/Style.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           FontAwesomeIcons.solidStar,
+          size: 14,
           color: Color(0xffFFDD4F),
         ),
         const SizedBox(
@@ -24,9 +27,12 @@ class BookRating extends StatelessWidget {
         const SizedBox(
           width: 5,
         ),
-        Text(
-          '(245)',
-          style: Styles.textStyle14.copyWith(color: Color(0xff707070)),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            '(245)',
+            style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+          ),
         )
       ],
     );
--- lib/Features/Home/presentaion/Views/widgets/detalse_view_body.dart
+++ lib/Features/Home/presentaion/Views/widgets/detalse_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/Features/Home/presentaion/Views/widgets/book_rating.dart';
 import 'package:bookly/Features/Home/presentaion/Views/widgets/custom_book_item.dart';
+import 'package:bookly/core/Utils/Style.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_book_detase_app_bar.dart';
@@ -15,9 +17,30 @@ class BookDetalseViewBody extends StatelessWidget {
         children: [
           const CustomBookDetalseAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: wight * .17),
+            padding: EdgeInsets.symmetric(horizontal: wight * .2),
             child: const CustomBookItem(),
           ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text(
+            'The Jungel Books',
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'The JunGel Books',
+              style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+            ),
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          )
         ],
       ),
     );
